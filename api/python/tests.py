f63b9274cff33f1114f59679e07e7d20ff04d7f3
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-08-16 01:30:05 +0300

    python api: add test runner

---
+++ api/python/tests.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2018 Yuriy Lisovskiy
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or https://opensource.org/licenses/MIT
+
+from .test.test_runner import run_tests
+
+if __name__ == '__main__':
+	run_tests()

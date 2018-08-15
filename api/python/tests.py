8c16dc86688e027fd9e9593b5a897b8259f27bad
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-08-16 01:32:09 +0300

    python api: fix build

--- api/python/tests.py
+++ api/python/tests.py
@@ -2,7 +2,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or https://opensource.org/licenses/MIT
 
-from .test.test_runner import run_tests
+from api.python.test.test_runner import run_tests
 
 if __name__ == '__main__':
 	run_tests()
